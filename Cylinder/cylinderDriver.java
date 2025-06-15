5f3651f19377a06c90096143d17bbda2558d4fa8
vinayakpop <vinayaksingh2172001@gmail.com>
2025-06-15 19:05:49 +0530

    cylinder code correct

--- Cylinder/cylinderDriver.java
+++ Cylinder/cylinderDriver.java
@@ -1,30 +1,33 @@
-class cylinderDriver{
-  public static void main(String []args);
-      cylinder c1 =new cylinder();
-      cylinder c2 =new cylinder();
-      cylinder c3 =new cylinder();
-      cylinder c4 =new cylinder();
+class CylinderDriver {
+    public static void main(String[] args) {
+        Cylinder c1 = new Cylinder();
+        Cylinder c2 = new Cylinder();
+        Cylinder c3 = new Cylinder();
+        Cylinder c4 = new Cylinder();
 
-      c1.setDetails(12.0, 5.0);
-      c2.setDetails(2.0, 1.0);
-      c3.setDetails(4.0, 3.0);
-      c4.setDetails(13.0, 6.0);
-     System.out.println(c1.getDetails());
-     System.out.println(c1.getTSA());
-     System.out.println(c1.getLSA());
-     System.out.println("===============*****============");
-      System.out.println(c2.getDetails());
-     System.out.println(c2.getTSA());
-     System.out.println(c2.getLSA());
-     System.out.println("===============*****============");
-      System.out.println(c3.getDetails());
-     System.out.println(c3.getTSA());
-     System.out.println(c3.getLSA());
-     System.out.println("===============*****============");
-      System.out.println(c4.getDetails());
-     System.out.println(c4.getTSA());
-     System.out.println(c4.getLSA());
-     System.out.println("===============*****============");
+        c1.setDetails(12.0, 5.0);
+        c2.setDetails(2.0, 1.0);
+        c3.setDetails(4.0, 3.0);
+        c4.setDetails(13.0, 6.0);
+
+        System.out.println(c1.getDetails());
+        System.out.println(c1.getTSA());
+        System.out.println(c1.getLSA());
+        System.out.println("===============*****============");
+
+        System.out.println(c2.getDetails());
+        System.out.println(c2.getTSA());
+        System.out.println(c2.getLSA());
+        System.out.println("===============*****============");
+
+        System.out.println(c3.getDetails());
+        System.out.println(c3.getTSA());
+        System.out.println(c3.getLSA());
+        System.out.println("===============*****============");
+
+        System.out.println(c4.getDetails());
+        System.out.println(c4.getTSA());
+        System.out.println(c4.getLSA());
+        System.out.println("===============*****============");
+    }
 }
-}
-    
