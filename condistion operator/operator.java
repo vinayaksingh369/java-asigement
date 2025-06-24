5e93695c3add90592c1643370a0e8d19de108541
vinayakpop <vinayaksingh2172001@gmail.com>
2025-06-25 00:46:05 +0530

    condistional

---
+++ condistion operator/operator.java
@@ -0,0 +1,8 @@
+class operator {
+    public static void main(String[] args) {
+        int x=3;
+        String result=(x %2== 0) ? "Even" : "Odd" ;
+        System.out.println(result);
+        
+    }
+}
